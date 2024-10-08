c335a350c38836e072d4ad6a45570ab2f624de03
Mathis <mathis.aupetit@fms-ea.com>
2024-10-08 10:58:30 +0200

    question 4

--- app_django/polls/admin.py
+++ app_django/polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    ordering = ["pub_date"]
+    date_hierarchy = "pub_date"
+    search_fields = ["question_text"]
+
+    list_filter = ["question_text"]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    ordering = ["question"]
+    date_hierarchy = "question__pub_date"
+    search_fields = ["choice_text","question__question_text"]
+
+    list_filter = ["question","question__question_text"]
+
+
+
---
+++ exo.md
@@ -0,0 +1,8 @@
+1. Ajout de ``admin.site.register(Choice)`` dans admin 
+2. .
+3. .
+    - Non
+    - Non
+    - Non
+    - Non
+4. 
